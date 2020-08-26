2569d4817e6b09f0a998e4c01823a82e38a23803
kogomar <kogomar@gmail.com>
2020-08-26 10:16:34 +0300

    Added Admin component

---
+++ code/assets/js/components/Admin.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Admin Dashboard
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Admin"
+}
+</script>
+
+<style scoped>
+
+</style>
--- code/assets/js/routes.js
+++ code/assets/js/routes.js
@@ -2,13 +2,15 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import Home from './components/Home';
+import Admin from './components/Admin';
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
     mode: 'history',
     routes:[
-        {path:'/', name:'home', component:Home}
+        { path:'/', name:'home', component: Home },
+        { path:'/admin', name:'admin', component: Admin }
     ]
 });
 
