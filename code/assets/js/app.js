9ec6f0090dc82113eb26d77c6965c9ea61989396
kogomar <kogomar@gmail.com>
2020-08-26 10:02:12 +0300

    Added axios globally

--- code/assets/js/app.js
+++ code/assets/js/app.js
@@ -4,11 +4,14 @@ import Vue from 'vue';
 import Vuetify from 'vuetify';
 import Routes from './routes.js';
 import App from './views/App';
+import Axios from 'axios'
 
+Vue.prototype.$axios = Axios;
 Vue.use(Vuetify);
 
 const app = new Vue({
     el: '#app',
+    vuetify : new Vuetify(),
     router: Routes,
     render: h => h(App),
 });
